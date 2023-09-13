b8a19c463d448f53f565bebe423d7bfb2cf0f7d2
jamil <jamilhossain3251@gmail.com>
2023-09-13 22:22:48 +0600

    update

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
 
 const userCollection = client.db('Netpay').collection('userCollection');
 const agentTransactionCollection = client.db('Netpay').collection('agentTransactions');
